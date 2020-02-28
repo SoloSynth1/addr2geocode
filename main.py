fc764b17917989ba9cde7193d092451fd96a67d5
Orix <orix.auyeung@hunt.social>
2020-02-28 17:54:39 +0800

    change output format

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 from geocode import Addr2Geocode
 
 location_file = "./data/locations.csv"
@@ -6,10 +8,13 @@ location_file = "./data/locations.csv"
 def get_addresses(location_file=location_file, skip_rows=1):
     addresses = []
     with open(location_file, 'r') as f:
-        for i, line in enumerate(f.readlines()):
+        reader = csv.reader(f)
+        for i, row in enumerate(reader):
             if i >= skip_rows:
-                address = line.split(",")[1]
-                addresses.append(address)
+                address = row[1]
+                district = row[3]
+                full_address = "{}, {}, {}".format("香港", district, address)
+                addresses.append(full_address)
     return addresses
 
 
@@ -17,9 +22,10 @@ if __name__ == "__main__":
     addresses = get_addresses()
     geocodes = []
     geocode_service = Addr2Geocode()
-    for address in addresses:
+    for i, address in enumerate(addresses):
+        print(i, address)
         lat, long = geocode_service.translate(address)
         geocodes.append((lat, long))
     print(geocodes)
-    with open("output.csv", 'w') as f:
-        f.write("\n".join(geocodes))
+    for geocode in geocodes:
+        print(geocode)
